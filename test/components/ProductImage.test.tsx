f913978450d54077722c8c74051f931363924363
nmarino <nicodemo.marino74@gmail.com>
2023-12-07 20:38:35 +0100

    Versión 0.0.1 lista

---
+++ test/components/ProductImage.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { ProductCard, ProductImage } from '../../src/components';
+import { product2 } from '../data/products';
+
+
+describe('ProductImage', () => {
+    test('debe mostrar el componente correctamente con la imagen personalizada', () => { 
+        const wrapper = renderer.create(
+            <ProductImage img='http://image1.jpg' />
+        )
+
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+
+    test('debe mostrar el componente con la imagen del producto', () => { 
+        const wrapper = renderer.create(
+            <ProductCard product={product2}>
+                {
+                    () => (
+                        <ProductImage/>
+                    )
+                }
+            </ProductCard>
+        )
+
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+ })
